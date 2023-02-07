dec3ec269a7afa3c3c2b98e3788983e9109133d9
sanjay <sanjaybelanekar07@gmail.com>
2023-02-07 15:54:53 +0530

    main to sanjay

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static("uploads"));
+app.use(express.static("v1/uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api/v1/users", require("./v1/routes/user.routes"));
 app.use("/api/v1/auth", require("./v1/routes/auth.routes"));
+app.use("/api/v1/tours", require("./v1/routes/tour.routes"));
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
