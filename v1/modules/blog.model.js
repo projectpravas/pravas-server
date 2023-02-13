5f12b7acd77e92b2f6d317405511d17133159c61
sanjay <sanjaybelanekar07@gmail.com>
2023-02-13 17:20:44 +0530

    blog controllers updated

--- v1/modules/blog.model.js
+++ v1/modules/blog.model.js
@@ -4,6 +4,15 @@ const AutoIncrement = require("mongoose-sequence")(mongoose);
 const BlogSchema = new mongoose.Schema({
   blogId: Number,
   title: String,
+  richText: String,
+  seoTitle: String,
+  metaDescription: String,
+  focusKeyphrases: String,
+  slug: String,
+  categories: Array,
+  tags: Array,
+  image: String,
+  blogStatus: Boolean,
 });
 
 BlogSchema.plugin(AutoIncrement, { inc_field: "blogId" });
